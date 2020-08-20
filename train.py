25ca93cdfde1c16cda2a99c73a7c9324d4418ee9
ChuanHao <33780183+AndrewCuHo@users.noreply.github.com>
2020-08-20 16:32:59 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
-from model import AlexNet
+import os
+from model import CRNN
 import numpy as np
 import pandas as pd
 import torch
@@ -13,22 +14,25 @@ from sklearn.metrics import average_precision_score
 from sklearn.metrics import log_loss
 from sklearn.metrics import roc_auc_score
 from Param_counter import get_model_complexity_info
+
 '''
 Test the process of data by using Attention-CNN method
 author: 112020333002191
 date: 20/8/19
 reference: https://github.com/AndrewCuHo/
 '''
-torch.cuda.set_device(0)
-torch.manual_seed(999)
+#
+RESUME = True
+eval = False
 
 # Hyper parameters
 num_epochs = 2000
 num_classes = 2
-batch_size = 150
-learning_rate = 6e-3
+batch_size = 50
+learning_rate = 3e-4
 weight_decay = 1e-4
-model = AlexNet().cuda()
+torch.cuda.set_device(0)
+torch.manual_seed(999)
 #################
 #  DataLoad   ###
 #################
@@ -38,8 +42,9 @@ df = pd.DataFrame(x)
 df['y'] = y
 X_train, X_test, y_train, y_test = train_test_split(
     x, y, test_size=0.2, random_state=999)
-X_train_valid = torch.from_numpy(X_train).float().unsqueeze(1) # dimension expand
-# 9156, 1 ,8
+X_train_valid = torch.from_numpy(X_train).float().unsqueeze(1)  # dimension expand
+# X_train_valid = X_train_valid.view(-1, 4, 4)
+# [9156, 1 , 8]
 y_train_valid = torch.from_numpy(y_train).long()
 train_data = torch.utils.data.TensorDataset(X_train_valid, y_train_valid)
 trainloader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
@@ -49,19 +54,39 @@ y_test = torch.from_numpy(y_test).long()
 test_data = torch.utils.data.TensorDataset(X_test, y_test)
 testloader = DataLoader(test_data, batch_size=X_test.shape[0], shuffle=False)
 
-writer = SummaryWriter('./tensorboard/cnn')
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), weight_decay=weight_decay, lr=learning_rate)
-t0 = time.time()
 def train_model():
+
+    model = CRNN().cuda()
+    writer = SummaryWriter('./tblogs/%f/' % learning_rate)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), weight_decay=weight_decay, lr=learning_rate)
+    lr_schedule = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[10, 20, 30, 40, 50], gamma=0.1)
+    t0 = time.time()
     cnt = 0
-    for epoch in range(1, num_epochs + 1):
+    total_step = len(trainloader)
+    hidden = None
+    class_correct = list(0. for i in range(num_classes))
+    class_total = list(0. for i in range(num_classes))
+    classes = ('True', 'False')
+    start_epoch = 0
+    ########################
+    ####断点重训########
+    if RESUME:
+        path_checkpoint = 'F:/EEG_data/MNE_test/code/reweights/checkpoint/%f/ckpt_best.pth' % (learning_rate)  # 断点路径
+        checkpoint = torch.load(path_checkpoint)  # 加载断点
+
+        model.load_state_dict(checkpoint['net'])  # 加载模型可学习参数
+
+        optimizer.load_state_dict(checkpoint['optimizer'])  # 加载优化器参数
+        start_epoch = checkpoint['epoch']  # 设置开始的epoch
+        lr_schedule.load_state_dict(checkpoint['lr_schedule'])
+
+    for epoch in range(start_epoch + 1, num_epochs + 1):
 
         # keep track of training
         train_loss = 0.0
         train_counter = 0
         train_losses = 0.0
-        total_step = len(trainloader)
         ###################
         # train the model #
         ###################
@@ -70,7 +95,9 @@ def train_model():
             data, target = data.cuda(), target.cuda()
             target = target.long()
             optimizer.zero_grad()
-            output = model(data)
+            output, hidden = model(data, hidden)
+            a, b = hidden
+            hidden = (a.data, b.data)
             loss = criterion(output, target)
             # print(target.data)
             loss.backward()
@@ -84,12 +111,28 @@ def train_model():
                 print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
                       .format(epoch + 1, num_epochs, cnt + 1, total_step, loss.item()))
         cnt = 0
-
-    torch.save(model.state_dict(), './model_EEG.pt')
+        checkpoint = {
+            "net": model.state_dict(),
+            'optimizer': optimizer.state_dict(),
+            "epoch": epoch,
+            'lr_schedule': lr_schedule.state_dict()
+        }
+        if not os.path.isdir("F:/EEG_data/MNE_test/code/reweights/checkpoint/%f" % (learning_rate)):
+            os.makedirs("F:/EEG_data/MNE_test/code/reweights/checkpoint/%f" % (learning_rate))
+        torch.save(checkpoint, 'F:/EEG_data/MNE_test/code/reweights/checkpoint/%f/ckpt_best.pth' % (learning_rate))
+        if epoch % 200 == 0:
+            torch.save(model.state_dict(), './model_EEG.pt')
+        #############
+        # eval
+        #############
+        #TODO add eval part
+    # torch.save(model.state_dict(), './model_EEG.pt')
     time_total = time.time() - t0
     print('Total time: {:4.3f}, average time per epoch: {:4.3f}'.format(time_total, time_total / num_epochs))
 
+
 def eval_model():
+    model = CRNN()
     model.load_state_dict(torch.load('./model_EEG.pt'))
     # specify the target classes
     classes = ('True', 'False')
@@ -102,10 +145,10 @@ def eval_model():
     model.eval()
     with torch.no_grad():
         for data, target in testloader:
-            data, target = data.cuda(), target.cuda()
+            data, target = data, target
             target = target.long()
-            output = model(data)
-            print(output.data)
+            output, _ = model(data)
+            #print(output.data)
             # convert output probabilities to predicted class
             _, pred = torch.max(output, 1)
             # print(pred)
@@ -120,10 +163,10 @@ def eval_model():
 
         data = next(iter(testloader))
         inputs, targets = data
-        inputs = inputs.cuda()
-        targets = targets.cuda()
+        inputs = inputs
+        targets = targets
         targets = targets.long()
-        outputs = model(inputs)
+        outputs, _ = model(inputs)
         probability, predicted = torch.max(outputs.data, 1)
         c = (predicted == targets).squeeze()
 
@@ -145,10 +188,9 @@ def eval_model():
 
 
 if __name__ == "__main__":
-    train_model()
+    if not eval :
+        train_model()
     eval_model()
-    #flops, params = get_model_complexity_info(model, (224, 224), as_strings=False, print_per_layer_stat=False)
-    #print('Flops:  %.3f' % (flops / 1e9))
-    #print('Params: %.2fM' % (params / 1e6))
-
-
+    # flops, params = get_model_complexity_info(model, (224, 224), as_strings=False, print_per_layer_stat=False)
+    # print('Flops:  %.3f' % (flops / 1e9))
+    # print('Params: %.2fM' % (params / 1e6))
